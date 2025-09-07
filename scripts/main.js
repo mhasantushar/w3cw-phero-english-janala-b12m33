e30a74ef16eec295ab50727d92295625269d536f
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-09-07 22:53:35 +0600

    learn vocab secte html done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,36 @@
 // console.log("Connected");
+
+const loadLessonButtons = () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => listLessonButtons(data.data))
+    .catch((err) => console.log("Error:", err));
+};
+
+const listLessonButtons = (lessons) => {
+  const buttonWrapper = document.getElementById("vocab-lesson-btns-wrapper");
+  // console.log (lessonButtonWrapper);
+  buttonWrapper.innerHTML = "";
+
+  lessons.forEach((lesson) => {
+    // console.log(lesson);
+    const buttonDiv = document.createElement("div");
+    buttonDiv.innerHTML = `
+                <button onclick="loadWordsForSelLesson(${lesson.level_no})"
+                class="vocab-lesson-btn btn btn-soft btn-primary">
+                <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+                `;
+    buttonWrapper.appendChild(buttonDiv);
+  });
+};
+
+const loadWordsForSelLesson = (selLevel) => {
+  const url = "https://openapi.programming-hero.com/api/level/"+selLevel;
+  // console.log (url);
+
+  document.getElementById("vocab-cards-missing").hidden = true;
+}
+
+loadLessonButtons();
