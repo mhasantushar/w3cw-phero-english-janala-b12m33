78736ad1b7ea3f7d367025aec28b639ae362d1e5
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-09-08 01:23:54 +0600

    learn vocab secte script primarily done

--- scripts/main.js
+++ scripts/main.js
@@ -27,10 +27,52 @@ const listLessonButtons = (lessons) => {
 };
 
 const loadWordsForSelLesson = (selLevel) => {
-  const url = "https://openapi.programming-hero.com/api/level/"+selLevel;
-  // console.log (url);
-
   document.getElementById("vocab-cards-missing").hidden = true;
-}
+
+  const url = "https://openapi.programming-hero.com/api/level/" + selLevel;
+  console.log(url);
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => displayWordsForSelLesson(data.data))
+    .catch((err) => console.log("Error:", err));
+};
+
+const displayWordsForSelLesson = (words) => {
+  // console.log (words);
+  const cardWrapper = document.getElementById("vocab-cards-wrapper");
+  cardWrapper.innerHTML = "";
+
+  words.forEach((word) => {
+    console.log(word);
+    const cardDiv = document.createElement("div");
+    // cardDiv.innerHTML = `
+    //       <article class="bg-white p-14 rounded-xl h-full">
+    //         <h3 class="mb-3 font-bold text-3xl">${word.word}</h3>
+    //         <h4 class="mb-8 font-medium text-lg">Meaning / Pronounciation</h4>
+    //         <h3 class="mb-16 font-bangla font-semibold text-3xl">${word.meaning} / ${word.pronunciation}</h3>
+    //         <div class="flex justify-between gap-4">
+    //           <button class="bg-[#1a91ff1a] hover:bg-[#1a91ff66] btn"><i class="fa-solid fa-circle-info"></i></button>
+    //           <button class="bg-[#1a91ff1a] hover:bg-[#1a91ff66] btn"><i class="fa-solid fa-volume-high"></i></button>
+    //         </div>
+    //       </article>`;
+    cardDiv.innerHTML = `
+          <article class="bg-white p-14 rounded-xl h-full">
+            <div class="flex flex-col justify-between">
+              <div>
+                <h3 class="mb-3 font-bold text-3xl">${word.word}</h3>
+                <h4 class="mb-8 font-medium text-lg">Meaning / Pronounciation</h4>
+                <h3 class="mb-16 font-bangla font-semibold text-3xl">${word.meaning} / ${word.pronunciation}</h3>
+              </div>
+              <div class="flex justify-between gap-4">
+                <button class="bg-[#1a91ff1a] hover:bg-[#1a91ff66] btn"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="bg-[#1a91ff1a] hover:bg-[#1a91ff66] btn"><i class="fa-solid fa-volume-high"></i></button>
+              </div>
+            </div>
+          </article>
+    `;
+    cardWrapper.appendChild(cardDiv);
+  });
+};
 
 loadLessonButtons();
